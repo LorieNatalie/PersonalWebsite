1e9c590c23ebc1442b22a10a7ebece51a608cbf8
Gloria <glndlovjhb024@student.wethinkcode.co.za>
2025-07-21 15:32:22 +0200

    Initial commit

---
+++ website.py
@@ -0,0 +1,12 @@
+#website.py
+
+from flask import Flask, render_template
+
+website = Flask(__name__)
+
+@website.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    website.run(debug=True)
