1ebe1ea0a2d3d5c790f37517e8fd94b405a3d2e8
Lorie <your_gitHub_lorienatalienoelle.com>
2025-07-24 14:07:02 +0200

    added a project and contact file

--- website.py
+++ website.py
@@ -8,5 +8,13 @@ website = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@website.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@website.route('contact')
+def contact():
+    return render_template('contact.html')
+
 if __name__ == '__main__':
     website.run(debug=True)
